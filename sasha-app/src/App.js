f15ea491e4776e807cc1ee08438436a50ef02311
Diana Murtazina <murka8831@yandex.ru>
2023-01-28 16:48:32 +0400

    header, localepicker

--- sasha-app/src/App.js
+++ sasha-app/src/App.js
@@ -1,10 +1,33 @@
+import { useState } from "react";
 import "./App.css";
+import { IntlProvider } from "react-intl";
+import NavBar from "./components/navbar/NavBar";
+import { LOCALES } from "./i18n/locales";
+import { messages } from "./i18n/messages";
+import GlobalContext from "./contexts/GlobalContext";
 
 function App() {
+    const [currentLocale, setCurrentLocale] = useState(
+        localStorage.getItem("language") || LOCALES.EN
+    );
     return (
-        <div className="App">
-            <header className="App-header">AA</header>
-        </div>
+        <GlobalContext.Provider value={{ currentLocale, setCurrentLocale }}>
+            <IntlProvider
+                onError={(err) => {
+                    if (err.code === "MISSING_TRANSLATION") {
+                        //console.warn("Missing translation", err.message);
+                        return;
+                    }
+                    throw err;
+                }}
+                messages={messages[currentLocale]}
+                locale={currentLocale}
+                defaultLocale={LOCALES.EN}>
+                <div className="App">
+                    <NavBar />
+                </div>
+            </IntlProvider>
+        </GlobalContext.Provider>
     );
 }
 
---
+++ sasha-app/src/i18n/messages.js
@@ -0,0 +1,16 @@
+import { LOCALES } from "./locales";
+
+export const messages = {
+    [LOCALES.EN]: {
+        home: "home",
+        about: "about",
+        projects: "projects",
+        contact: "contact",
+    },
+    [LOCALES.RU]: {
+        home: "главная",
+        about: "обо мне",
+        projects: "проекты",
+        contact: "контакты",
+    },
+};
