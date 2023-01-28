81843e8aeddcce2b3ac64615b3eebb0446bd3364
Diana Murtazina <murka8831@yandex.ru>
2023-01-28 15:26:17 +0400

    first commit

---
+++ sasha-app/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">AA</header>
+        </div>
+    );
+}
+
+export default App;
